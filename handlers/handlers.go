74b57efc9239381eec43386e3c2f5f37bbe3cdf4
skmalve <saikmr071@gmail.com>
2022-01-26 15:04:01 -0500

    Handling POST requests

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/SaiKumarMalve/Gator-News/middlew"
+	"github.com/SaiKumarMalve/Gator-News/routers"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
@@ -12,6 +14,8 @@ import (
 /* Handlers set the PORT for the servers to listen*/
 func Handlers() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/registration", middlew.CheckBD(routers.Registration)).Methods("POST")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
