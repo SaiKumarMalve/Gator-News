a1c1e361180af6d737098acb159d3398fcc47064
JayanthInv <jayantharinsen@gmail.com>
2022-01-26 16:37:10 -0500

    Left side modules

--- src/page/SignInSingUp/SignInSingUp.js
+++ src/page/SignInSingUp/SignInSingUp.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faSearch,faUsers,faComment} from "@fortawesome/free-solid-svg-icons";
 import "./SignInSingUp.scss";
 import LeftComponentBg from "../../assets/png/logo.png";
-
-
-
-
 export default function SignInSingUp() {
   return (
       <Container className="signin-signup" fluid>
@@ -18,20 +15,31 @@ export default function SignInSingUp() {
 
   );
 }
-
 function LeftComponent() {
   return (
-    <Col className="signin-signup__left">
+    <Col className="signin-signup__left" xs={6}>
+      <div>
+      <h2>
+          <FontAwesomeIcon icon={faSearch} />
+          Connect with Gators.
+        </h2>
         <h2>
-          Left Component .. Connect with Gators.
-        </h2> 
+          <FontAwesomeIcon icon={faUsers} />
+          Community to share your thoughts.
+        </h2>
+        <h2>
+          <FontAwesomeIcon icon={faComment} />
+          Get to know your Co-Gators.
+        </h2>
+      </div>
+      
     </Col>
   );
 }
 
 function RightComponent() {
   return (
-    <Col className="signin-signup__right">
+    <Col className="signin-signup__right" xs="6">
       <h2>Right Component ...</h2>
     </Col>
   );
