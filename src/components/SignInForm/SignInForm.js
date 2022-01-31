a2740d9bf07b936ee39e4bfc8c1eaf786bce0328
TEJASWINI1717 <kumkumtejaswini@gmail.com>
2022-01-31 15:17:50 -0500

    LoginFormComponents

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import {Form,Button,Spinner} from "react-bootstrap"
+import React, {useState} from 'react';
+import {Form,Button} from "react-bootstrap"
 import "./SignInForm.scss";
-export default function SignUpForm() {
+
+export default function SignInForm() {
+    const[formData,setFormData]=useState(initialFormValue());
     const onSubmit=e=>{
         e.preventDefault();
-        console.log("Login ..");
+        console.log(formData);
     };
-  return (
-      <div className="sign-in-form">
-          <h2>Enter Details</h2>
-          <Form onSubmit={onSubmit}>
-            <Form.Group> 
-                <Form.Control type="email" placeholder="Email address" />
+
+
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      };
+
+    return (
+        <div className="sign-in-form">
+          <h2>Gator Details</h2>
+          <Form onSubmit={onSubmit} onChange={onChange}>
+            <Form.Group>
+              <Form.Control
+                type="email"
+                placeholder="Email Address"
+                defaultValue={formData.email}
+                name="email"
+              />
             </Form.Group>
-            <Form.Group> 
-                <Form.Control type="password" placeholder="Enter Password"/>
+            <Form.Group>
+              <Form.Control
+                type="password"
+                defaultValue={formData.password}
+                name="password"
+                placeholder="Password"
+                
+              />
             </Form.Group>
             <Button variant="primary" type="submit">Login to Gator News </Button>
-        </Form>
-      </div>
-  )
-  };
+          </Form>
+        </div>
+      );
+    }
+  
+  
 
+  function initialFormValue() {
+      return{
+          email: "ktejaswini@gmail.com",
+          password: "123"
+      };
+  }
