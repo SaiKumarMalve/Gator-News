e3e733eeb0b8770395eb427e8f6f14b83322278b
TEJASWINI1717 <kumkumtejaswini@gmail.com>
2022-02-02 17:46:27 -0500

    Created Routing System for Home page and re-routing for other pages

---
+++ src/page/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Home.scss";
+
+export default function Home() {
+
+    return(
+        <div>
+            <h2>Welcome to Home Page</h2>
+        </div>
+    )
+}
