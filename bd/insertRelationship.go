15e166419e63570af8bd2c8714702bed071d4714
Alekhyagnv <alekhyagnv68@gmail.com>
2022-03-24 19:01:38 -0400

    Addressing the requested changes

--- bd/insertRelationship.go
+++ bd/insertRelationship.go
@@ -1,22 +1,23 @@
 package bd
 
-import(
+import (
 	"context"
 	"time"
-	"github.com/SaiKumarMalve/Gator-News/models"
 
+	"github.com/SaiKumarMalve/Gator-News/models"
 )
 
-func InsertRelationship(t models.Relationship) (bool,error){
-	ctx,cancel := context.WithTimeout(context.Background(),15*time.Second)
+/* This func inserts the relationship */
+func InsertRelationship(t models.Relationship) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	db := MongoCN.Database("gatorNews")
 	col := db.Collection("relationship")
 
-	_,err := col.InsertOne(ctx,t)
-	if err!=nil{
-		return false,err
+	_, err := col.InsertOne(ctx, t)
+	if err != nil {
+		return false, err
 	}
-	return true,nil
+	return true, nil
 }
