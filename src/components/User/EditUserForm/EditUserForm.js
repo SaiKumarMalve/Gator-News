c616e1fa3b607f38c78fe041ddc152fb2d362daa
TEJASWINI1717 <kumkumtejaswini@gmail.com>
2022-03-26 19:19:07 -0400

    Form Filling With Profile User

--- src/components/User/EditUserForm/EditUserForm.js
+++ src/components/User/EditUserForm/EditUserForm.js
@@ -7,11 +7,14 @@ import es from "date-fns/locale/es";
 import "./EditUserForm.scss";
 
 export default function EditUserForm(props) {
-  
-
+    const { user, setShowModal } = props;
+    
+    const [formData, setFormData] = useState(initialValue(user));
   const onSubmit = e => {
     e.preventDefault();   
     console.log("Editing User....");
+    console.log(formData);
+
   };
 
   return (
@@ -21,10 +24,10 @@ export default function EditUserForm(props) {
         <Form.Group>
           <Row>
             <Col>
-              <Form.Control type="text" placeholder="Name" name="name" />
+              <Form.Control type="text" placeholder="Name" name="name" defaultValue={formData.name} />
             </Col>
             <Col>
-              <Form.Control type="text" placeholder="Last Name" name="lastName" />
+              <Form.Control type="text" placeholder="Last Name" name="lastName" defaultValue={formData.lastName} />
             </Col>
           </Row>
         </Form.Group>
@@ -36,6 +39,7 @@ export default function EditUserForm(props) {
             placeholder="Add your biography"
             type="text"
             name="biography"
+            defaultValue={formData.biography}
           />
         </Form.Group>
 
@@ -44,6 +48,8 @@ export default function EditUserForm(props) {
             type="text"
             placeholder="Website"
             name="website"
+            defaultValue={formData.website}
+
           />
         </Form.Group>
 
@@ -63,3 +69,14 @@ export default function EditUserForm(props) {
     </div>
   );
 }
+
+function initialValue(user) {
+    return {
+      name: user.name || "",
+      lastName: user.lastName || "",
+      biography: user.biography || "",
+      location: user.location || "",
+      website: user.website || "",
+      dateOfBirth: user.dateOfBirth || "",
+    };
+  }
