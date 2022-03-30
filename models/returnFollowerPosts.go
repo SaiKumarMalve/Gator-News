812c2abb8bbd68904ebd55002bb9aaf7bac618a0
skmalve <saikmr071@gmail.com>
2022-03-30 17:15:50 -0400

    Fixed empty result from readFollowersPosts API

--- models/returnFollowerPosts.go
+++ models/returnFollowerPosts.go
@@ -10,7 +10,7 @@ type ReturnFollowersPosts struct {
 	ID                 primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
 	UserID             string             `bson:"userid" json:"userID,omitempty`
 	UserRelationshipID string             `bson:"userrelationshipid" json:"userRelationshipID,omitempty"`
-	Post               struct {
+	News               struct {
 		Message string    `bson:"message" json:"message,omitempty"`
 		Date    time.Time `bson:"date" json:"date,omitempty"`
 		ID      string    `bson:"_id" json:"_id,omitempty"`
