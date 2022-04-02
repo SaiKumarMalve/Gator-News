20eda438bb99c39a85cebdea31ab73bea6838d58
TEJASWINI1717 <kumkumtejaswini@gmail.com>
2022-04-01 23:47:53 -0400

    Jest Unit Cases for Front End

---
+++ src/page/Users/Users.test.js
@@ -0,0 +1,24 @@
+import { within, render, screen } from '@testing-library/react';
+import React from "react";
+import Users from './Users';
+import AllUsers from './Users';
+import { Router } from 'react-router-dom';
+
+import { MemoryRouter } from "react-router-dom";
+
+
+describe('AllUsers', () => {
+
+   
+    it("should render title", () => {
+        const view = render(
+            <MemoryRouter>
+              <AllUsers/>
+           </MemoryRouter>
+          );
+        expect(screen.queryAllByText("Users")).toHaveLength(2);
+      });
+      
+      
+
+});
